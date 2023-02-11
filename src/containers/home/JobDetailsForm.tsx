8a6ecee1ec8b22e25dff05c4d59499e207d2d9c3
Vivek K J <vivekkj2004@gmail.com>
2023-02-11 23:16:30 +0530

    Fix: Job Details next step not working

--- src/containers/home/JobDetailsForm.tsx
+++ src/containers/home/JobDetailsForm.tsx
@@ -20,7 +20,6 @@ const JobDetailsForm: React.FC<{
         jobTitle: Yup.string().required("Job Title is required"),
         jobDetails: Yup.string().required("Job Details is required"),
         jobLocation: Yup.string().required("Job Location is required"),
-        jobPosition: Yup.string().required("Job position is required"),
       }),
       onSubmit: (values) => {
         console.log({ values });
